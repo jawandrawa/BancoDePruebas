bf18ec44085dcad9a529c57a9a2bccaca7844734
Alejandra <alejandra_t00a@hotmail.com>
2024-07-02 18:51:06 +0200

    sensor client

---
+++ sensor_client/build/image_preload.cmake
@@ -0,0 +1,7 @@
+# Generated file that can be used to preload variant images
+set(BOARD "nrf52840dk_nrf52840" CACHE INTERNAL "NCS child image controlled")
+set(CACHED_CONF_FILE "c:/Users/aleja/Documents/teleco/tfg_conectividad/sensor_client/prj.conf" CACHE INTERNAL "NCS child image controlled")
+set(CONF_FILE_BUILD_TYPE "" CACHE INTERNAL "NCS child image controlled")
+set(DTC_OVERLAY_FILE "" CACHE INTERNAL "NCS child image controlled")
+set(NCS_TOOLCHAIN_VERSION "NONE" CACHE INTERNAL "NCS child image controlled")
+set(WEST_PYTHON "C:/ncs/toolchains/ffa23e2f3e/opt/bin/python.exe" CACHE INTERNAL "NCS child image controlled")
